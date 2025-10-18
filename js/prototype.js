ddc1187318926d42d4b7ae4dbd808b54577e98f1
ChenRepeat <aoyukisakura@gmail.com>
2025-10-18 23:27:48 +0800

    更新天地連結

--- js/prototype.js
+++ js/prototype.js
@@ -1,8 +1,14 @@
 // 導入頁首頁尾
-fetch('../header.html')
+// 自動判斷環境
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const basePath = isLocal ? '..' : '/Lexcy';
+
+fetch(`${basePath}/header.html`)
   .then(res => res.text())
   .then(data => document.querySelector('header').innerHTML = data)
+  .catch(err => console.error('載入 header 失敗:', err))
 
-fetch('../footer.html')
+fetch(`${basePath}/footer.html`)
   .then(res => res.text())
   .then(data => document.querySelector('footer').innerHTML = data)
+  .catch(err => console.error('載入 footer 失敗:', err))
