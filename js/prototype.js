1e4dc567f2e0894ff4c7cc217d6a997a4e740610
ChenRepeat <aoyukisakura@gmail.com>
2025-10-19 19:35:08 +0800

    修正天相關問題

--- js/prototype.js
+++ js/prototype.js
@@ -1,14 +1,17 @@
-// 導入頁首頁尾
-// 自動判斷環境
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-const basePath = isLocal ? '..' : '/Lexcy';
-
-fetch(`${basePath}/header.html`)
-  .then(res => res.text())
-  .then(data => document.querySelector('header').innerHTML = data)
-  .catch(err => console.error('載入 header 失敗:', err))
-
-fetch(`${basePath}/footer.html`)
-  .then(res => res.text())
-  .then(data => document.querySelector('footer').innerHTML = data)
-  .catch(err => console.error('載入 footer 失敗:', err))
+//漢堡
+const hamburger = document.querySelector('.hamburger-btn');
+const navMenu = document.querySelector('.navbar');
+  if (hamburger && navMenu) {
+    hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    if (navMenu.classList.contains('active')) {
+      hamburger.classList.remove('fa-bars');
+      hamburger.classList.add('fa-xmark');
+    } else {
+      hamburger.classList.remove('fa-xmark');
+      hamburger.classList.add('fa-bars');
+    }
+      });
+    } else {
+      console.error('Hamburger or menu not found');
+    }
