b9ea6ddd4b44484412b06c2b5d20f23b183af864
ChenRepeat <aoyukisakura@gmail.com>
2025-10-23 20:39:52 +0800

    完善景點物件、缺圖，確定jQuery函示庫有導入

--- js/tourism.js
+++ js/tourism.js
@@ -35,6 +35,78 @@ const cardsData = {
       content: `浯倆是一間坐落於金門的特色咖啡館，以結合在地文化與現代美學聞名。店名取自「浯島」之意，象徵金門人情與生活風景。空間設計溫潤簡約，融合老屋元素與文青氛圍，讓人感受悠閒時光。店內提供手沖咖啡、甜點與輕食，每一道皆展現對品質的堅持。無論是旅途中歇腳、與友人閒聊，或靜靜感受金門的慢節奏，浯倆都是理想的心靈休憩角落。`,
       located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d454.0648794390387!2d118.31672749979874!3d24.432771346052323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414a31d8e71d50b%3A0xe72eb53a8ce0aa8f!2z5rWv5YCG54mb6IKJ6bq1772c6YeR6ZaA5oiw5Zyw5ruL5ZGz772c6YeR6ZaA576O6aOf!5e0!3m2!1szh-TW!2stw!4v1761193961791!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
   },
+  7:{
+      title: '沙溪堡',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `沙溪堡位於金門西南方，為昔日重要的海防據點之一，見證金門戰地歷史。堡體依地勢而建，結合坑道、碉堡與射口設計，具備堅固防禦功能。現今開放為觀光景點，遊客可沿途欣賞壯麗海景與軍事遺跡，感受當年戰地氣息。沙溪堡不僅是軍事史的重要見證，也展現金門從戰地走向和平的歷史轉折與文化價值。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3633.132442840354!2d118.21385488732696!3d24.411465969447786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34149ea411640189%3A0xa1679a8ec8908263!2z5rKZ5rqq5aCh!5e0!3m2!1szh-TW!2stw!4v1761219980491!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  8:{
+      title: '北山播音牆',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `北山播音牆位於金門北山村，為冷戰時期著名的心理戰設施之一。當年以高功率擴音設備，對對岸進行宣傳與廣播，傳遞自由與和平訊息。牆體設計宏大、外觀堅固，現今已成為極具象徵意義的戰地文化景點。遊客可近距離觀察當年設備遺跡，感受兩岸對峙時的歷史氛圍與時代印記，見證金門特殊的戰地文化記憶。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1815.4506694059048!2d118.3129229995426!3d24.48887509331588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2stw!4v1761220108179!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  9:{
+      title: '勇士堡',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `勇士堡位於金門烈嶼（小金門）西南方，為昔日重要的防禦據點之一。堡內設有坑道、觀測口與射擊孔，結構堅固且依地勢隱蔽，充分展現戰地工事的巧思。昔日駐軍在此監控海面動態，守護金門安全。如今勇士堡開放為觀光景點，遊客可探訪坑道遺跡，欣賞壯闊海景，感受當年軍人堅守前線的英勇精神與戰地歲月的歷史氛圍。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d908.0272955250115!2d118.25168996616212!3d24.446997004548194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414991df2a8747b%3A0x6d5e7ed85a865561!2z5YuH5aOr5aCh!5e0!3m2!1szh-TW!2stw!4v1761220141334!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  10:{
+      title: '金東電影院',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `金東電影院位於金門金沙鎮，建於戰地時期，是當年官兵與居民重要的娛樂場所。以簡樸卻堅固的建築風格見證當時的軍事文化生活。當年在緊張的駐防歲月裡，電影成為紓解壓力與聯繫情感的重要媒介。院名由于右任題字，原為軍中熱門戲院，現登錄歷史建築，修繕後成為觀光景點，曾為電影《軍中樂園》拍攝地，遊客可探索內部展覽與金門歷史風貌。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1815.5979703615526!2d118.42610541137168!3d24.47866691742789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414bb302d831ebf%3A0x6884ce306f43722a!2z6YeR5p2x6Zu75b2x6Zmi!5e0!3m2!1szh-TW!2stw!4v1761220171684!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  11:{
+      title: '馬山觀測所',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `馬山觀測站位於金門北端，地理位置面向廈門，是昔日最前線的重要軍事觀測據點。觀測站設有狹長觀景孔與堅固掩體，用於監視對岸動態與情報蒐集。站內仍保留軍事設施與解說展示，重現戰地時期的緊張氛圍。登上觀測點可遠眺廈門市景與海峽風光，體驗昔日前線與現今和平的強烈對比，象徵金門從戰地走向和平的重要歷史意義。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3629.7793568735565!2d118.40437278885496!3d24.527716100000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414bb9a23a574c1%3A0xc949bd751977fc42!2z6aas5bGx6KeA5ris5omA!5e0!3m2!1szh-TW!2stw!4v1761220197733!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  12:{
+      title: '歐厝沙灘戰車',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `歐厝沙灘位於金門西南方，為昔日兩棲登陸演訓的重要地點之一。沙灘上陳列著退役戰車與軍事遺構，展現濃厚的戰地風情。昔日此地為軍事重地，如今則成為結合歷史與自然景觀的特色景點。金黃沙灘與碧海相映，搭配戰車剪影，構成獨特的金門風景。遊客可近距離感受戰地歷史氛圍，同時欣賞海岸的壯闊與寧靜。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3633.4118804319037!2d118.33107881247997!3d24.401754520046627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414a226e94bdb77%3A0xf984f05e72dab298!2z5q2Q5Y6d5rKZ54GY!5e0!3m2!1szh-TW!2stw!4v1761220255387!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  13:{
+      title: '雙口海灘',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `雙口海灘位於金門烈嶼鄉西南方，擁有細緻沙灘與遼闊海景，是兼具自然風光與歷史記憶的景點。此地曾為軍事防線之一，岸邊仍可見碉堡與障礙樁遺跡，見證戰地歲月。如今軍事氣息淡去，取而代之的是寧靜與廣闊的海岸風光。黃昏時分，夕陽映照海面，景色壯麗動人，是漫步觀海與拍照取景的絕佳去處。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3632.33252980492!2d118.2264293762862!3d24.439245761981315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34149938b4daf34d%3A0xd1cc5973b1e908e7!2z6ZuZ5Y-j5rW36YKK!5e0!3m2!1szh-TW!2stw!4v1761220280575!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  14:{
+      title: '高洞（E092據點）',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `高洞（E092據點）位於金門塔后地區，為昔日重要的沿海防禦據點之一。據點依地勢興建，設有坑道、射口與觀測孔，構造堅固且隱蔽性高。昔日駐軍在此監控海面動態，守護金門南岸安全。現今高洞經整修開放，保留原始軍事設施與解說導覽，並可遠眺壯麗海景，讓遊客在探訪中體驗前線據點的歷史氛圍，感受金門從戰地走向和平的時代印記。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d3632.507951812355!2d118.4043979643312!3d24.43315615399384!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjTCsDI1JzU5LjkiTiAxMTjCsDI0JzI0LjYiRQ!5e0!3m2!1szh-TW!2sus!4v1761220490082!5m2!1szh-TW!2sus" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  15:{
+      title: '山后民俗文化村',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `山后民俗文化村位於金門金沙鎮，為保存最完整的閩南傳統聚落之一。建於清光緒年間，由十八棟古厝組成，格局嚴謹、雕飾精緻，展現金門富庶家族的建築美學與生活風貌。村內保存豐富的民俗文物與生活器具，讓人一窺昔日鄉村的文化底蘊。如今山后民俗文化村已成為金門代表性的文化景點，融合建築之美與歷史記憶，展現閩南傳統生活的深厚魅力。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1815.2299964923711!2d118.4389537751675!3d24.504160588100554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414bb3000000001%3A0xf12924a4cf03c34a!2z6YeR6ZaA5bGx5ZCO5rCR5L-X5paH5YyW5p2R!5e0!3m2!1szh-TW!2stw!4v1761220525446!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  16:{
+      title: '燕南書院',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `金門燕南書院創建於清代，為當地文風鼎盛的重要學府。昔日士子在此聚集讀書講學，不僅承擔教育功能，更推動地方文化傳承與士風培養。書院建築典雅，保存傳統格局，展現書香氛圍。現今燕南書院不僅是金門珍貴的人文資產，更成為遊客體驗古風與了解地方教育歷史的重要文化景點。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1816.6314393583766!2d118.31225140030554!3d24.40693331062912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414a2028361fabb%3A0xd119ea9808ece8de!2z54eV5Y2X5pu46Zmi!5e0!3m2!1szh-TW!2stw!4v1761220551675!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  17:{
+      title: '料羅港',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `料羅港位於金門西南沿海，為重要的天然港口與歷史貿易據點。港區水域寬闊，昔日不僅方便漁業捕撈，也作為軍事防禦與物資運輸的樞紐。沿岸保留部分古老碼頭與防護設施，見證金門歷史發展與戰地歲月。如今料羅港結合觀光與休閒功能，遊客可欣賞海景、體驗漁港風情，感受金門沿海生活與歷史文化交織的獨特魅力。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3633.094945205456!2d118.42809068540117!3d24.41276886953898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5paZ576F5riv!5e0!3m2!1szh-TW!2stw!4v1761220589850!5m2!1szh-TW!2stw" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
+  18:{
+      title: '古崗湖',
+      imgurl: './img/tour-detail-pic1.png',
+      content: `古崗湖位於金門西南部，是一處天然淡水湖泊，兼具生態與歷史價值。湖面寬廣、環境清幽，周邊蘆葦叢生，吸引多種候鳥棲息，形成豐富的生態景觀。昔日湖區亦曾作為軍事防守地帶的一部分，保留部分戰地遺跡。如今古崗湖開放遊憩，遊客可沿湖散步、觀鳥或欣賞湖光山色，體驗金門自然美景與戰地歷史的交融氛圍。`,
+      located: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1816.811346349018!2d118.31468813906119!3d24.39442564271205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3414a1ee15941f73%3A0x4364a21f173b75e1!2z5Y-k5bSX5rmW!5e0!3m2!1szh-TW!2stw!4v1761220626871!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
+  },
 };
 
 function getCardIdFromURL() {
